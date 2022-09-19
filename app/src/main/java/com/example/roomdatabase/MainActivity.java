2ad2ab38b51685190a0e41c91e30450c291009d6
rumaan <rummaanumme@gmail.com>
2022-09-19 16:34:01 +0530

    Room Database -1

--- app/src/main/java/com/example/roomdatabase/MainActivity.java
+++ app/src/main/java/com/example/roomdatabase/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,11 +21,39 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         initViews();
         mDb = AppDatabase.getInstance(getApplicationContext());
+        intent = getIntent();
+        if (intent != null && intent.hasExtra(Constants.UPDATE_Person_Id)) {
+            button.setText("Update");
+
+            mPersonId = intent.getIntExtra(Constants.UPDATE_Person_Id,-1);
+            AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                @Override
+                public void run() {
+                    Person person = mDb.personDao().loadPersonById(mPersonId);
+                    populateUI(person);
+                }
+            });
+        }
+
+    }
+
+    private void populateUI(Person person) {
+        if (person == null) {
+            return;
+        }
+        name.setText(person.getName());
+        email.setText(person.getEmail());
+        phoneNumber.setText(person.getNumber());
+        pincode.setText(person.getPincode());
+        city.setText(person.getCity());
     }
 
     private void initViews() {
@@ -51,11 +82,30 @@ public class MainActivity extends AppCompatActivity {
         AppExecutors.getInstance().diskIO().execute(new Runnable() {
             @Override
             public void run() {
-                mDb.personDao().insertPerson(person);
+                if(!intent.hasExtra(Constants.UPDATE_Person_Id)){
+                    mDb.personDao().insertPerson(person);
+                }
+                else {
+                    person.setId(mPersonId);
+                    mDb.personDao().updatePerson(person);
+
+                }
+                finish();
+
             }
         });
     }
-}
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
 
 
